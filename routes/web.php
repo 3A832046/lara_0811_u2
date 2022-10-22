d0413bc2aeb5f64c112e2e825d45d7b6bc5f1eb7
Chen Ching Ling <3a832046@gm.student.ncut.edu.tw>
2022-10-22 23:48:06 +0800

    設定Route前置

--- routes/web.php
+++ routes/web.php
@@ -39,3 +39,12 @@ Route::get('hello/{name?}',function($name='Everybody'){
 Route::get('aa/',function(){
     return'welcome';
 });
+
+Route::get('dashboard',function(){
+    return'dashboard';
+});
+Route::group(['prefix'=>'admin'],function(){
+    Route::get('dashboard',function(){
+        return'admin dashboard';
+    });
+});
